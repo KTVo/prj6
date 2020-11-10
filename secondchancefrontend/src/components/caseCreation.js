3bf0ab4567ab51ab32f0335527ce065141fcedca
Eric Diaz <eric.diaz13@yahoo.com>
2020-11-10 15:37:59 -0800

    Removed hardcoding

--- secondchancefrontend/src/components/caseCreation.js
+++ secondchancefrontend/src/components/caseCreation.js
@@ -22,7 +22,7 @@ export class CaseCreation extends React.Component
                 people1: [],
                 patientRecords: [],
                 is_rec_loading: true,
-                record_id: 1
+                record_id: this.props.userInfo.record_id
             };
         this.data = {
             pat_email: '',  //For identifying who the case belongs to
@@ -282,7 +282,7 @@ export class CaseCreation extends React.Component
             body: JSON.stringify( {
                 record_id: this.state.record_id,
                 physician_id: selectedDr.drId,
-                pat_id: 1 // this.props.userInfo.pat_id
+                pat_id: this.props.userInfo.pat_id // this.props.userInfo.pat_id
 
             })
         }
